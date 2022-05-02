a6be3379adb82d12b7413ce32475945526470570
raki123 <rafael.kiesel@web.de>
2022-05-02 11:35:09 +0200

    Added static switch to setup script.

--- setup.sh
+++ setup.sh
@@ -3,7 +3,11 @@ mkdir bin
 rm -rf build
 mkdir build
 cd build 
-cmake ..
+if [ "$1" = "static" ]; then
+        cmake -DSTATIC=ON ..
+else
+        cmake ..
+fi
 make -j4
 cp defined ../bin/
 cp minisat/minisat ../bin/
